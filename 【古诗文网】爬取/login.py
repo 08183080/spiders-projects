63315895f77d26d19ada2a20dc9e3ef59332bad7
龙龙 <51738561+08183080@users.noreply.github.com>
2023-11-05 21:33:05 +0800

    【增强】验证码识别

--- 【古诗文网】爬取/login.py
+++ 【古诗文网】爬取/login.py
@@ -1,20 +1,71 @@
 import requests
+import base64
+import json
+from lxml import etree
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-    "Cookie": "wxopenid=defoaltid; ticketStr=202553309%7cgQFU8DwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyVzExMlF2bGVkN2kxdXFVRnhCMVEAAgSaa0JlAwQAjScA; Hm_lvt_9007fab6814e892d3020a64454da5a55=1696932163,1698850241,1699116144; login=flase; Hm_lpvt_9007fab6814e892d3020a64454da5a55=1699150341; ASP.NET_SessionId=yn5tju4r0s2ez3d0o1ci4wrq; codeyzgswso=573734c5f363c5ac"
-}
+"""
+【目的】通过打码平台识别验证码, 模拟登录【古诗文】网
+【获取目的url】(1) f12, (2) network, (3) preserve logs, (4) all 
+"""
 
-
-def login(url):
+def get_code(image, verify_type="60000"):
     """
-    【目的】用cookie方式模拟登录【古诗文网】
-    【进度】初次运行, 未扫码...
+    获取验证码
     """
-    resp = requests.get(url = url, headers = headers).content.decode()
-    print(resp)
+    _custom_url = "http://api.jfbym.com/api/YmServer/customApi"
+    _token = "aMRurUVe_aQ_66ejmWaMVeFnJy6MkTo02mUYrHI_gdI"
+    _headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    payload = {
+        "image": base64.b64encode(image).decode(),
+        "token": _token,
+        "type": verify_type
+    }
+
+    # print(payload)
+    resp = requests.post(_custom_url, headers=_headers,data=json.dumps(payload))
+    # print(resp.text)
+    return resp.json()['data']['data']
+
 
 if __name__ == "__main__":
-    url = "https://so.gushiwen.cn/user/getEventLogin.aspx?scene_id=202553309"
-    login(url)
+
+    # 1. 将验证码保存本地
+    url = "https://so.gushiwen.cn/user/login.aspx?from=http://so.gushiwen.cn/user/collect.aspx"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    }
+    page_text = requests.get(url=url, headers=headers).text
+
+    tree = etree.HTML(page_text)
+    code_img_src ="https://so.gushiwen.cn/" + tree.xpath('/html/body/form[1]/div[4]/div[4]/img/@src')[0]
+    img_data = requests.get(url=code_img_src, headers=headers).content
+    with open('./code.png','wb') as f:
+        f.write(img_data)
     
+    # 2. 调用打码平台示例程序进行验证码识别
+    # 2.1 我们自己封装整理出一个识别验证码的函数
+    code_text = get_code(image=img_data, verify_type="10110")
+    print('识别结果: ', code_text)
+
+    # 2.2 导入其他文件中的类【其他的方法】
+    # y = YdmVerify()
+    # y.common_verify(img_data,10110) 
+
+    login_url = "https://so.gushiwen.cn/user/login.aspx?from=http%3a%2f%2fso.gushiwen.cn%2fuser%2fcollect.aspx"
+    data = {
+        "__VIEWSTATE": "kgCPZcmBWciXXasiCxE7SctULlln4O+k98VKRAWxZ+/4eoyvGjYbpVEpuWc6cYrG0o19/bAUzzCVBEbDDTHaAM1uN/+H1oEDRnGDRDKRZkHOVLXzim78bORTqa4a5sYzH2zpFagM/bB+LInuWdOqHt8iwS8=",
+        "__VIEWSTATEGENERATOR": "C93BE1AE",
+        "from": "http://so.gushiwen.cn/user/collect.aspx",
+        "email": "xxxx",
+        "pwd": "xxx",
+        "code": code_text,
+        "denglu": "登录"
+    }
+
+    resp = requests.post(url=login_url, headers=headers, data=data)
+    print(resp.status_code)
+    print(resp.text)
+    # 返回200，则是响应成功...
